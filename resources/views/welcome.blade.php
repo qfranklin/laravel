68cbf4c947a60854d9a16010f4b408635922a01d
Quentin Franklin <qfranklin906@gmail.com>
2024-11-03 17:41:31 -0500

    removed css mix from welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel with Vue</title>
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    <div id="app">
-        <example-component></example-component>
-    </div>
-
-    <script src="{{ mix('js/app.js') }}"></script>
+    <div id="app"></div>
+    <script src="/js/app.js"></script>
 </body>
 </html>

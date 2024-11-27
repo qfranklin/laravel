6dbcfdb5f52cf154b3f9407d477975d1876191d0
Quentin Franklin <qfranklin906@gmail.com>
2024-11-27 12:39:03 -0500

    removed unused endpoints, functions, queries

--- app/Models/BitcoinPrice.php
+++ app/Models/BitcoinPrice.php
@@ -21,35 +21,6 @@ class BitcoinPrice extends Model
         'volume',
     ];
 
-    public static function calculateSMA($date, $window)
-    {
-        return self::where('date', '<=', $date)
-            ->orderBy('date', 'desc')
-            ->take($window)
-            ->avg('close_price');
-    }
-
-    public static function getSMAData($startDate, $endDate)
-    {
-        return self::whereBetween('date', [$startDate, $endDate])
-            ->orderBy('date')
-            ->get()
-            ->map(function ($item) {
-                $item->sma_50 = self::calculateSMA($item->date, 50);
-                $item->sma_200 = self::calculateSMA($item->date, 200);
-                return $item;
-            });
-    }
-
-    public static function getQuarterlyData($year, $startMonth, $endMonth)
-    {
-        return self::whereYear('date', $year)
-            ->whereMonth('date', '>=', $startMonth)
-            ->whereMonth('date', '<=', $endMonth)
-            ->orderBy('date')
-            ->get();
-    }
-
     public static function calculateMovingAverage($date, $days)
     {
         $subQuery = self::select('close_price')

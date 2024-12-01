acef124f2727a2b649db02d8e0f3c925c6739b16
Quentin Franklin <qfranklin906@gmail.com>
2024-12-01 18:46:34 -0500

    reverted kernel scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,12 +7,18 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    protected function schedule(Schedule $schedule)
+    /**
+     * Define the application's command schedule.
+     */
+    protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('fetch:bitcoin-price')->dailyAt('03:00');
+        // $schedule->command('inspire')->hourly();
     }
 
-    protected function commands()
+    /**
+     * Register the commands for the application.
+     */
+    protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
 
